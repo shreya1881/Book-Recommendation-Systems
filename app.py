929c3de0df064af4f8f648661a7f325efffbb5da
shreya1881 <2704shreya@gmail.com>
2022-09-26 23:41:39 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+import pickle
+popular_df=pickle.load(open('popular.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                            book_name = list(popular_df['Book-Title'].values),
+                             author = list(popular_df['Book-Author'].values),
+                              image = list(popular_df['Image-URL-M'].values),
+                             votes = list(popular_df['num_ratings'].values),
+                             rating = list(popular_df['avg_ratings'].values))
+
+# @app.route('/recommend')
+# def recommend_ui():
+#     return render_template('recommend.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
